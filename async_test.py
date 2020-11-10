aff4754ea54163637f27f3586f0bc505a79c3a8c
Eldar <eldar100@mail.ru>
2020-11-10 18:27:17 +0300

    fix

--- async_test.py
+++ async_test.py
@@ -2,47 +2,42 @@ import asyncio
 import time
 
 
-class async_to_sync:
-    def __init__(self, f):
-        self.f = f
-
-    def __call__(self, *args, **kwargs):
-        asyncio.get_event_loop().create_task(self.f(*args, **kwargs))
-
-
-async def test1(x):
+async def test(name, x):
     for i in range(x):
-        print(i)
-        await asyncio.sleep(0.01)
+        print(name, i)
+        # time.sleep(i)
+        await asyncio.sleep(1)
 
 
-async def test2(x):
-    k = 0
-    while True:
-        if k >= 3:
-            break
-        print("---", k)
-        await asyncio.sleep(0.5)
-        k += 1
+task_obj: asyncio.Task = None
 
 
-counter = 0
+async def handler(name, x):
+    global task_obj
+    if bool(x % 2):
+        task_obj = asyncio.create_task(test(name, x))
+    else:
+        print("Try cancel", task_obj)
+        if task_obj is not None:
+            print("Cancel", task_obj.cancelled())
+            task_obj.cancel()
 
-start = time.time()
+
+async def main(loop):
+    print("start")
+    await handler('A', 7)
+    await asyncio.sleep(2)
+    await handler('B', 2)
+    await asyncio.sleep(2)
+    await handler('B', 2)
+    await asyncio.sleep(2)
+    await handler('C', 9)
 
 
-async def main(x):
-    global counter
-
-    for i in range(x):
-        if time.time() - start >= 1:
-            break
-        # print(i)
-        counter += 1
-        await asyncio.sleep(0)
-
-    print(counter)
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main(100000))
+
+loop.run_until_complete(main(loop))
+
+loop.run_forever()
--- data_analys.py
+++ data_analys.py
@@ -1,7 +1,20 @@
 import pandas as pd
+
 file = "aes_data.xlsx"
 
-data = pd.read_excel(file)
+# data = pd.read_excel(file)
 
-print(data.head())
+# print(data.head())
+
+from iapws import IAPWS97, IAPWS95
+
+sat_steam = IAPWS97(P=1, x=1)
+sat_liquid = IAPWS97(T=300, x=0)  # saturated liquid with known T
+steam = IAPWS97(P=0.1, T=300)  # steam with known P and T
+
+water1 = IAPWS97(P=1, T=380)
+water2 = IAPWS95(P=1, T=380)
+
+print(water1.rho, water1.phase)
+print(water2.rho, water2.phase)
 
