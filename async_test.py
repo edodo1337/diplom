fa56682a143591eecf33cd1edf7860b59bebdd29
Eldar <eldar100@mail.ru>
2020-11-11 21:49:57 +0300

    fix

--- async_test.py
+++ async_test.py
@@ -1,43 +1,57 @@
-import asyncio
+# import asyncio
+# import time
+#
+#
+# async def test(name, x):
+#     for i in range(x):
+#         print(name, i)
+#         # time.sleep(i)
+#         await asyncio.sleep(1)
+#
+#
+# task_obj: asyncio.Task = None
+#
+#
+# async def handler(name, x):
+#     global task_obj
+#     if bool(x % 2):
+#         task_obj = asyncio.create_task(test(name, x))
+#     else:
+#         print("Try cancel", task_obj)
+#         if task_obj is not None:
+#             print("Cancel", task_obj.cancelled())
+#             task_obj.cancel()
+#
+#
+# async def main(loop):
+#     print("start")
+#     await handler('A', 7)
+#     await asyncio.sleep(2)
+#     await handler('B', 2)
+#     await asyncio.sleep(2)
+#     await handler('B', 2)
+#     await asyncio.sleep(2)
+#     await handler('C', 9)
+#
+#
+#
+#
+# loop = asyncio.get_event_loop()
+#
+# loop.run_until_complete(main(loop))
+#
+# loop.run_forever()
+
+
+from tqdm import tqdm
+from loguru import logger
 import time
 
+logger.remove()
+logger.add(lambda msg: tqdm.write(msg, end=""))
 
-async def test(name, x):
-    for i in range(x):
-        print(name, i)
-        # time.sleep(i)
-        await asyncio.sleep(1)
+logger.info("Initializing")
 
-
-task_obj: asyncio.Task = None
-
-
-async def handler(name, x):
-    global task_obj
-    if bool(x % 2):
-        task_obj = asyncio.create_task(test(name, x))
-    else:
-        print("Try cancel", task_obj)
-        if task_obj is not None:
-            print("Cancel", task_obj.cancelled())
-            task_obj.cancel()
-
-
-async def main(loop):
-    print("start")
-    await handler('A', 7)
-    await asyncio.sleep(2)
-    await handler('B', 2)
-    await asyncio.sleep(2)
-    await handler('B', 2)
-    await asyncio.sleep(2)
-    await handler('C', 9)
-
-
-
-
-loop = asyncio.get_event_loop()
-
-loop.run_until_complete(main(loop))
-
-loop.run_forever()
+for x in tqdm(range(100)):
+    # logger.info("Iterating #{}", x)
+    time.sleep(0.1)
