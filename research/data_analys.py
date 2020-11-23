00bf4aebaae8d306da7eac8ca9e00dec7db3c18d
edodo1337 <eldar100@mail.ru>
2020-11-23 23:10:55 +0300

    fix

--- research/data_analys.py
+++ research/data_analys.py
@@ -19,3 +19,11 @@ print(water1.rho, water1.phase)
 print(water2.rho, water2.phase)
 
 
+
+
+'pump_1_amper': pd.concat([df[pump_amper_cols[0]][i[0]:i[1]] for i in segments1],  ignore_index=True),
+'pump_2_amper': pd.concat([df[pump_amper_cols[1]][i[0]:i[1]] for i in segments2],  ignore_index=True),
+'pump_3_amper': pd.concat([df[pump_amper_cols[2]][i[0]:i[1]] for i in segments3],  ignore_index=True),
+'pump_4_amper': pd.concat([df[pump_amper_cols[3]][i[0]:i[1]] for i in segments4],  ignore_index=True),
+'pump_5_amper': pd.concat([df[pump_amper_cols[4]][i[0]:i[1]] for i in segments5],  ignore_index=True),
+  
