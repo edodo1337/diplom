54ce8871c26e6bd5a380f3f325be8cc016553d2d
edodo1337 <eldar100@mail.ru>
2020-12-18 21:18:16 +0300

    fix

--- research/data_analys.py
+++ research/data_analys.py
@@ -1,34 +1,16 @@
-import pandas as pd
+
+
+def test(x):
+  print("A",x)
 
 
 
-a = {
-  '1': (2, 4, 8),
-  '2': (4, 8, 16)
-}
+def calc():
+  test(5)
 
-b = {
-  '1': (3, 9, 27),
-  '2': (6, 18, 54)
-}
+calc()
 
+def test(x):
+  print("B", x)
 
-
-from itertools import chain
-
-
-d_values = [i.values() for i in [a,b]]
-
-x1 = [i[0] for i in chain(*d_values)]
-x2 = [i[1] for i in chain(a.values(), b.values())]
-x3 = [i[2] for i in chain(a.values(), b.values())]
-
-
-data = {
-  'first': x1,
-  'second': x2,
-  'third': x3,
-}
-
-df = pd.DataFrame(data)
-print(df)
+calc()
